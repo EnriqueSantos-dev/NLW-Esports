ae9df519072ddef623e4cf545894685d8a94da3b
Enrique Santos <santosenrique2121@gmail.com>
2022-09-12 23:47:17 -0300

    chore: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+function Button() {
+  return (
+    <button
+      className="rounded px-4 py-2 flex justify-center items-center text-zinc-800 bg-zinc-200"
+      type="button"
+    >
+      Botão teste
+    </button>
+  );
+}
+
+export default Button;
